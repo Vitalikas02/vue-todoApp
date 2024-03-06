ca81aeef60f5c96a78a33bae7224f7af9a9a3390
Vitalikas02 <vitalik_vasilev_03@mail.ru>
2024-03-06 16:50:54 +0300

    Initial commit

---
+++ src/components/TaskInput.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="task-input my-list">
+        <input v-model="title" placeholder="Название" type="text">
+        <input v-model="description" placeholder="Описание" type="text">
+        <button @click="onAddTask">Добавить задачу</button>
+    </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+    emits: {
+        onAddTask({ title, description }) {
+            if (title == '' || description == '') {
+                alert('Пожалуйста, заполните все поля')
+                return false
+            }
+            return true
+        }
+    },
+    setup(props, { emit }) {
+        const title = ref('')
+        const description = ref('')
+
+        const onAddTask = () => {
+            emit('onAddTask', { title: title.value, description: description.value })
+        }
+
+        return {
+            title,
+            description,
+            onAddTask
+        }
+    }
+}
+</script>
+
+<style scoped>
+.task-input {
+    margin: 10px 0;
+}
+</style>
